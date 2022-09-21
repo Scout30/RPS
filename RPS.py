063e1b9cd1417026ee0b2a49a2d63399a4ddf830
Scout30 <93880884+Scout30@users.noreply.github.com>
2022-09-21 10:17:07 +0300

    Update RPS.py

--- RPS.py
+++ RPS.py
@@ -1,17 +1,18 @@
 import random
-
-player = input("enter your choice: ")
+from timeit import repeat
 choice = ["paper", "rock", "scissors"]
 
-cpu = random.choice(choice)
-
-if player == cpu:
-    print('Draw!')
-elif player == 'parer' and cpu == 'rock':
-    print('Pleyer wins!')
-elif player == 'scissors' and cpu == 'paper':
-    print('Pleyer wins!')
-elif player == 'rock' and cpu == 'scissors':
-    print('Pleyer wins!')
-else:
-    print('CPU wins!')
+repeat:
+    player = input("enter your choice: ")
+    cpu = random.choice(choice)
+    if player == cpu:
+        print('Draw!')
+    elif player == 'parer' and cpu == 'rock':
+        print('Pleyer wins!')
+    elif player == 'scissors' and cpu == 'paper':
+        print('Pleyer wins!')
+    elif player == 'rock' and cpu == 'scissors':
+        print('Pleyer wins!')
+    else:
+        print('CPU wins!')
+       
